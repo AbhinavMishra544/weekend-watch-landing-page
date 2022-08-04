c039861678675c11184fa55d75e0f42812a53c2c
=abhinav <=abhinavmishra544@gmail.com>
2022-08-04 21:06:25 +0530

    added movie card

--- src/components/Trending.tsx
+++ src/components/Trending.tsx
@@ -8,6 +8,7 @@ type MovieSliderProp = {
 
 const Trending : React.FC = () => {
   const [trendingList, setTrendingList] = useState([]);
+  const [selectedWatchOption, setSelectedWatchOption] = useState('streaming');
 
     useEffect(()=>{
         const getList = async () => {
@@ -18,13 +19,19 @@ const Trending : React.FC = () => {
     },[]);
 
     return (
-        <div  className='trending-list'>
-            <div>
-                <span> What’s Popular </span>
-                <span></span>
+        <div className='trending-list'>
+            <div className='whats-popular-option'>
+                <span className='popular-title-text'> What’s Popular </span>
+                <div className='watch-category'>
+                    <div  className={`${selectedWatchOption === 'streaming' ? 'selected' : ''} watch-item`} onClick={()=>setSelectedWatchOption('streaming')}>
+                        Streaming
+                    </div>
+                    <div className={`${selectedWatchOption === 'ontv' ? 'selected' : ''} watch-item`} onClick={()=>setSelectedWatchOption('ontv')}>
+                        On TV
+                    </div>
+                </div>
             </div>
             <MovieSlider trendingList={trendingList}/>
-
         </div>
     )
 }
@@ -33,17 +40,16 @@ const MovieSlider : React.FC <MovieSliderProp> = ({ trendingList })  => {
     return (
         <Carousel
             cols={6}
-            gap={10}
-            containerStyle={{margin:'0px', padding:'0px'}}
-      >{
-        trendingList && trendingList.map((item)=>{
+            gap={16}
+            containerStyle={{margin:'0px', padding:'2.378rem 9.125rem 3rem 9.125rem'}}
+        >
+        {trendingList && trendingList.map((item)=>{
             return (
                 <Carousel.Item>
                 <MovieCard info={item}/>
             </Carousel.Item>
             )
-        })
-      }
+        })}
   </Carousel>
     )
 }
@@ -58,12 +64,31 @@ const MovieCard: React.FC <any> = ({ info }) =>  {
          result += formattedDate.toLocaleString( 'default', { year : 'numeric' } );
         return result;
     }
+    const RenderStarRating: React.FC <any> = (props) : any => {
+        const { ratingInfo } = props;
+        const stars = [];
+        for(let i=0;i<ratingInfo.vote_average/2 ; i++){
+            stars.push(
+                <img
+                    src='./../star.svg'
+                    height={15}
+                    width={16}
+                    alt={ratingInfo.title}
+                />
+            )
+        }
+        return <div style={{'display':'flex', 'marginTop':'4px'}}> {stars} </div>
+
+    }
 
     return(
         <div className='movie-card'>
-            <img src={`https://image.tmdb.org/t/p/w440_and_h660_face${info.backdrop_path}`} className={'movie-img-card'} />
-            <label>{info.title}</label>
-            <div>{formatDate(info.release_date)}</div>
+            <img src={`https://image.tmdb.org/t/p/w440_and_h660_face${info.backdrop_path}`} className={'movie-img-card'} alt={info.title}/>
+            <div className='movie-card-details'>
+                <label className='movie-card-title'>{info.title}</label>
+                <label className='movie-release-date'>{formatDate(info.release_date)}</label>
+                {info.vote_average ? <RenderStarRating ratingInfo={ (info) }/> : null}
+            </div>
         </div>
     )
 }
